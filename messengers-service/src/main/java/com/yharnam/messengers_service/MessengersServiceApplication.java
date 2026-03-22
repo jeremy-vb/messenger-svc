4007b0fd3e8be1ec4dc282741e2fc7918d49e7d3
jeremyVenegas <jeremyvenegasb@gmail.com>
2026-03-22 12:37:31 -0600

    initial commit

---
+++ messengers-service/src/main/java/com/yharnam/messengers_service/MessengersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yharnam.messengers_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengersServiceApplication.class, args);
+	}
+
+}
---
+++ messengers-service/src/test/java/com/yharnam/messengers_service/MessengersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yharnam.messengers_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
